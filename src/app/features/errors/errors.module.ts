1a5284f62a38b82e99e81e441d9a5cda3bfd996d
mgzur <mgzurakowski@gmail.com>
2022-05-19 01:52:16 +0200

    feat: create Something Went Wrong error page

--- src/app/features/errors/errors.module.ts
+++ src/app/features/errors/errors.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ErrorsRoutingModule } from 'src/app/features/errors/errors-routing.module';
 import { GeolocationErrorComponent } from './pages/geolocation-error/geolocation-error.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SomethingWentWrongErrorComponent } from './pages/something-went-wrong-error/something-went-wrong-error.component';
 
 
 
 @NgModule({
   declarations: [
-    GeolocationErrorComponent
+    GeolocationErrorComponent,
+    SomethingWentWrongErrorComponent
   ],
   imports: [
     CommonModule,
